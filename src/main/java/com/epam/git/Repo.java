9f38f3729f6b69982a35b3f0d38d8f129664e447
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-12-17 23:19:19 +0400

    second attempt

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Matseonzhek/stage2-module1-git.git";
+    public static String REPO_LINK = "https://github.com/Matseonzhek/GitPractice";
 }
